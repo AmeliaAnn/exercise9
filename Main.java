901b30789863d309df9457bb6614ee834f40f662
AmeliaAnn <53219102+AmeliaAnn@users.noreply.github.com>
2019-10-22 17:07:17 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,18 +4,24 @@ import java.util.*;
 
 public class Main {
   
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
     Scanner a = new Scanner(System.in);
 	System.out.println("Введите длину массива: ");
 		int n = a.nextInt();
-		int b[]=new int[n];
+		int b[] = new int[n];
 		System.out.println("Заполните массив:");
 		int i = 0;
-		while (i < b.length){
+		while (i < b.length)  
+		   {
 			b[i] = a.nextInt();
-			i++;}
-            System.out.println("Массив: ");
-			System.out.println(Arrays.toString(b));
+			i++;
+		  }   
+   for (i = 0; i < b.length; i += 0)
+      b[i] *= 2;
+    System.out.println("Массив, умноженный на 2:");
+    for (i = 0; i < b.length; i++)
+    System.out.println(Arrays.toString(b));
    
    
   }
